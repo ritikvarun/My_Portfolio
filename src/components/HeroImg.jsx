2a312b6150bb3b1f5b35eac60164274d31b150ff
ritik varun <your.email@example.com>
2025-07-09 01:22:09 +0530

    Describe your changes here

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -7,11 +7,11 @@ const Card = () => {
       options={{ max: 25, scale: 1.05, speed: 400 }}
       className="flex justify-center items-center mt-10 cursor-pointer"
     >
-      <div className="bg-[#121212] border p-4 rounded-2xl w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+      <div className="bg-[#121212] border p-4 rounded-2xl w-[250jpx] max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
         <img 
-          src="/EMS.png" // Ensure this path is correct
+          src="/ritik.JPG" // Ensure this path is correct
           alt="React Native"
-          className="rounded-2xl mb-4 w-full h-auto object-cover"
+          className="rounded-2xl mb-4 w-[250px]  h-auto object-cover"
         />
         <h2 className="text-white text-lg font-bold">JS Developer</h2>
       </div>
